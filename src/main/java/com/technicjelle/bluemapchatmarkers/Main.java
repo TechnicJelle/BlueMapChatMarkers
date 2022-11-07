473db51210ed5d5a764b27c4739de9fef5fc0063
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-11-07 02:30:12 +0100

    Re-Architecture how markers and sets are added

--- src/main/java/com/technicjelle/bluemapchatmarkers/Main.java
+++ src/main/java/com/technicjelle/bluemapchatmarkers/Main.java
@@ -3,12 +3,13 @@ package com.technicjelle.bluemapchatmarkers;
 import com.flowpowered.math.vector.Vector3d;
 import de.bluecolored.bluemap.api.BlueMapAPI;
 import de.bluecolored.bluemap.api.BlueMapMap;
+import de.bluecolored.bluemap.api.BlueMapWorld;
 import de.bluecolored.bluemap.api.markers.HtmlMarker;
 import de.bluecolored.bluemap.api.markers.MarkerSet;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -17,20 +18,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import java.util.function.Consumer;
 
 @SuppressWarnings("unused")
 public final class Main extends JavaPlugin implements Listener {
 
-	private final int seconds = 60;
+	private final int seconds = 60; //TODO: Make this configurable
 
-	private String style;
+	private String style; //This is configurable in the style.css file
 
-	private Map<UUID, MarkerSet> markerSets;
+	private final String MARKERSET_ID = "chat-markers"; //No need to make this configurable
 
 	@Override
 	public void onEnable() {
@@ -60,51 +57,56 @@ public final class Main extends JavaPlugin implements Listener {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		markerSets = new HashMap<>();
-
-		//loop through all worlds on the server
-		List<World> worlds = Bukkit.getWorlds();
-		for (World world : worlds) {
-			//make a new markerset for this world
-			MarkerSet markerSet = new MarkerSet("Chat Markers");
-			markerSet.setDefaultHidden(false);
-			markerSet.setToggleable(true);
-
-			//add the markerset to all BlueMap maps of this world
-			api.getWorld(world).ifPresent(bmWorld -> {
-				for (BlueMapMap map : bmWorld.getMaps()) {
-					map.getMarkerSets().put("chat-markers", markerSet);
-				}
-			});
-
-			//add the markerset to the global world,markerset collection, so it can be accessed later
-			markerSets.put(world.getUID(), markerSet);
-
-			//make a marker that the loads in the style to be used for all the chat markers
-			HtmlMarker marker = new HtmlMarker("MarkerStyleLoader", new Vector3d(0, 0, 0), "<style>" + style + "</style>");
-
-			markerSet.getMarkers().put("style-loader", marker);
-		}
 	};
+
+	private void createMarkerSet(BlueMapWorld bmWorld) {
+		getLogger().info("Creating MarkerSet for BlueMap World " + bmWorld.getId());
+		MarkerSet markerSet = new MarkerSet("Chat Markers");
+		markerSet.setDefaultHidden(false);
+		markerSet.setToggleable(true);
+
+		//add the markerset to all BlueMap maps of this world
+		for (BlueMapMap map : bmWorld.getMaps()) {
+			map.getMarkerSets().put(MARKERSET_ID, markerSet);
+		}
+
+		//make a marker that the loads in the style to be used for all the chat markers
+		HtmlMarker marker = new HtmlMarker("MarkerStyleLoader", new Vector3d(0, 0, 0), "<style>" + style + "</style>");
+
+		markerSet.put("style-loader", marker);
+	}
 
 	@EventHandler
 	public void onPlayerChat(AsyncPlayerChatEvent event) {
 		BlueMapAPI.getInstance().ifPresent(api -> {
-			Location location = event.getPlayer().getLocation();
+			Player player = event.getPlayer();
+			BlueMapWorld bmWorld = api.getWorld(player.getWorld()).orElse(null);
+			if (bmWorld == null) return; //world not loaded in BlueMap, ignore
+//			if (!api.getWebApp().getPlayerVisibility(player.getUniqueId())) return; //player hidden on BlueMap, ignore //TODO: Turn this back on once BlueMap fixes this
 
-			HtmlMarker marker = new HtmlMarker(event.getPlayer().getName() + ": " + event.getMessage(),
+			Location location = player.getLocation();
+
+			HtmlMarker marker = new HtmlMarker(player.getName() + ": " + event.getMessage(),
 					new Vector3d(location.getX(), location.getY(), location.getZ()),
 					"<div class='chatMarker'>" + event.getMessage() + "</div>");
 
-			markerSets.get(event.getPlayer().getWorld().getUID()).getMarkers()
-					.put(String.valueOf(event.hashCode()), marker);
+			//for all BlueMap Maps belonging to the BlueMap World the Player is in, add the Marker to the MarkerSet of that BlueMap World
+			bmWorld.getMaps().forEach(map -> {
+				if(!map.getMarkerSets().containsKey(MARKERSET_ID)) //if this world doesn't have a MarkerSet yet, create it
+					createMarkerSet(bmWorld); //creates a new MarkerSet, and assigns it to each Map of this World
 
-			//wait seconds and remove the marker
-			Bukkit.getScheduler().runTaskLater(this, () ->
-				markerSets.get(event.getPlayer().getWorld().getUID()).getMarkers()
-						.remove(String.valueOf(event.hashCode())),
-				20 * seconds);
+				MarkerSet markerSet = map.getMarkerSets().get(MARKERSET_ID);
+
+				String key = String.valueOf(event.hashCode());
+
+				//add Marker to the MarkerSet
+				markerSet.put(key, marker);
+
+				//wait Seconds and remove the Marker
+				Bukkit.getScheduler().runTaskLater(this,
+						() -> markerSet.remove(key),
+						seconds * 20);
+			});
 		});
 	}
 
